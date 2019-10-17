e0c3eb9ea4f5a78cdfc2e12ab5635385559080e7
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-17 20:37:14 +0900

    809

--- overlay.js
+++ overlay.js
@@ -20,17 +20,23 @@ var seido=8;
 var canvas = document.getElementById('canvas');
     canvas.width=wid;
     canvas.height=hei;
+var canvas_alpha = document.getElementById('canvas_alpha');
+    canvas_alpha.width=wid;
+    canvas_alpha.height=hei;
     // そのまま表示すると鏡像にならないので反転させておく
     //canvas.style.transform = 'rotateY(180deg)';
     document.getElementById("video").style.visibility="visible"
     document.getElementById("plane").style.display="none"
+    document.getElementById("plane_alpha").style.display="none"
     canvas.style.visibility="visible"
+    canvas_alpha.style.display="none"
     //autoplayを外し、playを入れたらcanvasに描がかれた
     document.getElementById('plane').play();
+    document.getElementById('plane_alpha').play();
     document.getElementById('video').play();
 
 var context = canvas.getContext('2d');
-
+var context_alpha = canvas_alpha.getContext('2d');
 
 
 const medias =
@@ -84,6 +90,8 @@ function errorCallback(err) {
     {
         context.clearRect(0, 0, wid, hei);
         context.drawImage(plane,rectX,rectY,rectWid,rectHei,rectX,rectY,rectWid,rectHei);
+        context_alpha.clearRect(0, 0, wid, hei);
+        context_alpha.drawImage(plane_alpha,rectX,rectY,rectWid,rectHei,rectX,rectY,rectWid,rectHei);
         detect();
         requestAnimationFrame(draw);
     };
@@ -94,6 +102,8 @@ function errorCallback(err) {
     {
         var imageData = context.getImageData(rectX,rectY,rectWid,rectHei);
         data = imageData.data;
+        var imageData2 = context_alpha.getImageData(rectX,rectY,rectWid,rectHei);
+        data2 = imageData2.data;
         // dataはUint8ClampedArray
         // 長さはcanvasの width * height * 4(r,g,b,a)
         // 先頭から、一番左上のピクセルのr,g,b,aの値が順に入っており、
@@ -109,15 +119,14 @@ function errorCallback(err) {
                     g: data[i + 1],
                     b: data[i + 2]
                 };
-            var black =
+            var back =
             　　{
-                    r: 0,
-                    g: 0,
-                    b: 0
+                    r: data2[i],
+                    g: data2[i + 1],
+                    b: data2[i + 2]
                 };
-            console.log("detect");
             //blackは消す
-            if (target == black)
+            if (back.r  == 0 && back.g == 0 back.b == 0)
               {
               data[i + 3] = 0;
               }
@@ -130,8 +139,9 @@ function errorCallback(err) {
 
 console.log(target);
 
-
     }
+
+
 
 
 
