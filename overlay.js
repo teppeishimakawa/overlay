9c70ac9c37a58dd3d9125ac63c229dfd6a34d091
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-17 19:59:21 +0900

    759

--- overlay.js
+++ overlay.js
@@ -1,10 +1,33 @@
 
+//var colorDistance=10;
+//var seido=4;
 
+
+var wid=window.innerWidth;
+var hei=window.innerHeight;
+
+var rectX=0;
+var rectY=0;
+var rectWid=1280;
+var rectHei=720;
+
+
+
+
+//input video
+var canvas = document.getElementById('canvas');
+    canvas.width=wid;
+    canvas.height=hei;
+    // そのまま表示すると鏡像にならないので反転させておく
+    //canvas.style.transform = 'rotateY(180deg)';
     video.style.visibility="visible"
-
+    canvas.style.visibility="visible"
     //autoplayを外し、playを入れたらcanvasに描がかれた
-    document.getElementById('video').play();
-    document.getElementById('over').play();
+    document.getElementById('plane').play();
+
+var context = canvas.getContext('2d');
+
+
 
 const medias =
 {
@@ -32,7 +55,6 @@ navigator.mediaDevices = navigator.mediaDevices ||
 if(navigator.mediaDevices)
 {
 
-
 const promise = navigator.mediaDevices.getUserMedia(medias);
 
 promise.then(successCallback)
@@ -41,6 +63,7 @@ promise.then(successCallback)
 
 function successCallback(stream) {
   video.srcObject = stream;
+  draw();
  };
 
 function errorCallback(err) {
@@ -51,4 +74,59 @@ function errorCallback(err) {
 {
 
 }
+
+    // planeの映像をcanvasに描画する
+    function draw()
+    {
+        context.clearRect(0, 0, wid, hei);
+        context.drawImage(over,rectX,rectY,rectWid,rectHei,rectX,rectY,rectWid,rectHei);
+        detect();
+        requestAnimationFrame(draw);
+    };
+
+
+    // 検出処理
+    function detect()
+    {
+        var imageData = context.getImageData(rectX,rectY,rectWid,rectHei);
+        data = imageData.data;
+        // dataはUint8ClampedArray
+        // 長さはcanvasの width * height * 4(r,g,b,a)
+        // 先頭から、一番左上のピクセルのr,g,b,aの値が順に入っており、
+        // 右隣のピクセルのr,g,b,aの値が続く
+        // n から n+4 までが1つのピクセルの情報となる
+
+
+        for (var i=0; i < data.length; i += seido)
+         　{
+            var target =
+            　　{
+                    r: data[i],
+                    g: data[i + 1],
+                    b: data[i + 2]
+                };
+            var black =
+            　　{
+                    r: 0,
+                    g: 0,
+                    b: 0
+                };
+
+            //blackは消す
+            if (target == black)
+              {
+              data[i + 3] = 0;
+              }
+            else
+              {
+              return;
+              }
+        　 }
+
+         }
+
+
+
+
+
 
