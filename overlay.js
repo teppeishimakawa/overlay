45537dcab4e1b8e033eee16e0625037c64c38cdc
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-17 21:50:08 +0900

    835

--- overlay.js
+++ overlay.js
@@ -128,7 +128,7 @@ function errorCallback(err) {
                 //console.log(target);
                 //console.log(back.r);
             //blackは消す
-            if (data2[i]  < 5)
+            if (back.r)
               {
               data[i + 3] = 0;
               }
